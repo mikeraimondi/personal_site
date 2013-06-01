2fa5d9c39d008c22467953b49214a4bb0747db71
Michael Raimondi <zeno@pobox.com>
2013-05-31 23:50:12 -0400

    Refactor sidebar and implement basic contact page
    Refactor sidebar from JS to server-side logic for active class change.
    Tweak sidebar behavior on home page. Implement placeholder contact page.

--- app.rb
+++ app.rb
@@ -2,23 +2,32 @@
 
 require 'sinatra'
 
+@home = false
+@current = ''
+
 get '/' do
+  @home = true
+  @current = 'home'
   erb :index
 end
 
 get '/about' do
+  @current = 'about'
   erb :about
 end
 
 get '/portfolio' do
+  @current = 'portfolio'
   erb :portfolio
 end
 
 get '/resume' do
+  @current = 'resume'
   erb :resume
 end
 
 get '/contact' do
+  @current = 'contact'
   erb :contact
 end
 
--- public/javascripts/main.js
+++
@@ -1,5 +0,0 @@
-$(function () {
-  var currentPageId = "#" + $(".nav-active").attr("id");
-  $("ul.side-nav li").removeClass("active");
-  $("ul.side-nav li" + currentPageId + "_nav").addClass("active");
-});
