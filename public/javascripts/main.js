e00a985f995842d8f87203f8e240c11e71c0d369
Michael Raimondi <zeno@pobox.com>
2013-06-30 15:01:29 -0400

    Implement about and portfolio pages

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,10 +1,43 @@
-$(function(){
+$(function () {
 
   var $container = $('#portfolio-container');
 
   $container.isotope({
-    layoutMode: 'masonry',
-    itemSelector: '.portfolio-item'
+    itemSelector: '.portfolio-item',
+    layoutMode: 'fitRows',
+    getSortData: {
+      featured: function ($elem) {
+        return $elem.attr('data-featured');
+      }
+    }
+  });
+
+  $('#featured-btn').click(function () {
+    $('#portfolio-container').isotope({ filter: '.featured'});
+  });
+
+  $('#rails-btn').click(function () {
+    $('#portfolio-container').isotope({ filter: '.rails'});
+  });
+
+  $('#ruby-btn').click(function () {
+    $('#portfolio-container').isotope({ filter: '.ruby'});
+  });
+
+  $('#js-btn').click(function () {
+    $('#portfolio-container').isotope({ filter: '.javascript'});
+  });
+
+  $('#sinatra-btn').click(function () {
+    $('#portfolio-container').isotope({ filter: '.sinatra'});
+  });
+
+  $('#csharp-btn').click(function () {
+    $('#portfolio-container').isotope({ filter: '.csharp'});
+  });
+
+  $('#all-btn').click(function () {
+    $('#portfolio-container').isotope({ filter: '*'});
   });
 
 });
